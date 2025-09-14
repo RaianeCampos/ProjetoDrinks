c4e95dbc7d65c13380322f83f4c54b207a80afc7
RaianeCampos <raianecampos2002@gmail.com>
2025-09-13 21:40:15 -0300

    Nova atualização do App

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,71 @@ import { CocktailProvider } from './contexts/CocktailContext';
 import CocktailActions from './components/CocktailActions';
 import CocktailDisplay from './components/CocktailDisplay';
 import { Container, Typography, CssBaseline, Box } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#431a1a', 
+    },
+    primary: {
+      main: '#251515ff',
+    },
+    secondary: {
+      main: '#251515ff',
+    },
+    text: {
+      primary: '#251515ff',
+      
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, sans-serif',
+  },
+});
 
 function App() {
   return (
-    <CocktailProvider>
-      <CssBaseline />
-      <Container maxWidth="md">
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h2" component="h1" align="center" gutterBottom>
-            Guia de Drinks
-          </Typography>
-          <Typography variant="subtitle1" align="center" color="text.secondary" sx={{ mb: 4 }}>
-            Projeto de Programação Web Fullstack [cite: 3]
-          </Typography>
-          <CocktailActions />
-          <CocktailDisplay />
+    
+    <ThemeProvider theme={theme}>
+      <CocktailProvider>
+        <CssBaseline />
+        <Box
+          sx={{
+            minHeight: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            py: 4, 
+          }}
+        >
+          <Container maxWidth="lg">
+            <Box
+              sx={{
+                backgroundColor: '#ecdec5ff',
+                p: { xs: 2, sm: 4 },
+                borderRadius: 2,
+                boxShadow: 3,
+              }}
+            >
+              <Typography variant="h2" component="h1" align="center" gutterBottom color="primary">
+                Guia de Drinks
+              </Typography>
+              <Typography variant="subtitle1" align="center" color="text.secondary" sx={{ mb: 4 }}>
+                Aproveite e relaxe com um bom drink!
+              </Typography>
+
+              <CocktailActions />
+              <CocktailDisplay />
+            </Box>
+          </Container>
         </Box>
-      </Container>
-    </CocktailProvider>
+      </CocktailProvider>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
